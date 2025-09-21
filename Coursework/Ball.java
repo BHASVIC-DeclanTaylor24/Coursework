8cb7af70ba0f2c721f4c13315c7669babc7ca9a5
BHASVIC-DeclanTaylor24 <declan.taylor24@students.bhasvic.ac.uk>
2025-09-21 23:01:00 +0100

    Started my coursework ball class the write up
    added the global variables and made a method for acceleration and velocity

---
+++ Coursework/Ball.java
@@ -0,0 +1,40 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Ball here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Ball extends Actor
+{
+    double velocity =0;
+    int mass =0;
+    int rampAngle = 45;
+    float friction = 0;
+    boolean startButton = false;
+    int time;
+    /**
+     * Act - do whatever the Ball wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        if (startButton == true){
+            time++;
+            velocity();
+            
+        
+        }
+    }
+    public double acceleration(){
+        double acceleration;
+        acceleration = mass*9.8*Math.sin(rampAngle) - friction;
+        return acceleration;
+    }
+    public double velocity(){
+        velocity = velocity + acceleration() * (time/60);
+        return velocity;
+    }
+}
