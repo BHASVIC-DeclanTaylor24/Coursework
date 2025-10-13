23f8dff482c83f809ad528564c0eb986d738e0d0
Declan Taylor <declan.taylor24@students.bhasvic.ac.uk>
2025-10-13 13:59:40 +0100

    Making it so the ramp actually is connected

--- Coursework/MyWorld.java
+++ Coursework/MyWorld.java
@@ -8,6 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
+    int rampAngle = 45;
 
     /**
      * Constructor for objects of class MyWorld.
@@ -19,8 +20,11 @@ public class MyWorld extends World
         super(600, 400, 1); 
         Ramp ramp1 = new Ramp(0);
         addObject(ramp1,115,320);
-        Ramp ramp2 = new Ramp(65);
-        addObject(ramp2,147,253);
+        Ramp ramp2 = new Ramp(rampAngle);
+        addObject(ramp2,215,320);
         
+    }
+    public int getRampAngle(){
+        return rampAngle;
     }
 }
--- Coursework/Ramp.java
+++ Coursework/Ramp.java
@@ -8,6 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Ramp extends Actor
 {
+    
     /**
      * Act - do whatever the Ramp wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -22,5 +23,21 @@ public class Ramp extends Actor
         getImage().setColor(Color.BLACK);
         getImage().fillRect(0,0,200,10);
         setRotation(rampAngle);
+        setLocation(getX() - (int)xSide(), getY() + (int)ySide());
+        
     }
+    public double xSide(){
+        double x;
+        MyWorld myWorld;
+        x = 50 * Math.cos(myWorld.getRampAngle());
+        return(x);
+    }
+    public double ySide(){
+        double y;
+        MyWorld myWorld;
+        y = 50 * Math.sin(myWorld.getRampAngle());
+        return(y);
+    }
+    
+    
 }
